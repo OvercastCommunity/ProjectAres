08085ed983fb066382cece5b6bee36001e6f92e8
Indre <hic.3t.nunc@gmail.com>
2019-08-22 10:05:58 +0200

    Remove unused imports in ReportCommands

--- Commons/bukkit/src/main/java/tc/oc/commons/bukkit/report/ReportCommands.java
+++ Commons/bukkit/src/main/java/tc/oc/commons/bukkit/report/ReportCommands.java
@@ -1,10 +1,7 @@
 package tc.oc.commons.bukkit.report;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.WeakHashMap;
-import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -13,71 +10,36 @@ import com.sk89q.minecraft.util.commands.CommandContext;
 import com.sk89q.minecraft.util.commands.CommandException;
 import com.sk89q.minecraft.util.commands.CommandPermissions;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TranslatableComponent;
-import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.Player;
 import java.time.Duration;
 import java.time.Instant;
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerQuitEvent;
-import tc.oc.api.bukkit.users.BukkitUserStore;
-import tc.oc.api.docs.Report;
-import tc.oc.api.docs.Server;
 import tc.oc.commons.bukkit.channels.AdminChannel;
-import tc.oc.commons.core.chat.Audience;
-import tc.oc.minecraft.scheduler.SyncExecutor;
-import tc.oc.api.model.QueryService;
 import tc.oc.commons.bukkit.chat.Audiences;
-import tc.oc.commons.bukkit.commands.UserFinder;
-import tc.oc.commons.bukkit.nick.IdentityProvider;
 import tc.oc.commons.core.chat.Component;
-import tc.oc.commons.core.commands.CommandFutureCallback;
 import tc.oc.commons.core.commands.Commands;
 import tc.oc.commons.core.commands.TranslatableCommandException;
 import tc.oc.commons.core.formatting.PeriodFormats;
 import tc.oc.commons.core.util.Comparables;
 import tc.oc.minecraft.api.event.Listener;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 @Singleton
 public class ReportCommands implements Commands, Listener {
 
-    private final QueryService<Report> reportService;
     private final ReportCreator reportCreator;
     private final ReportConfiguration reportConfiguration;
-    private final UserFinder userFinder;
-    private final SyncExecutor syncExecutor;
-    private final Server localServer;
-    private final BukkitUserStore userStore;
     private final Audiences audiences;
-    private final IdentityProvider identities;
     private final AdminChannel adminChannel;
     private final Map<CommandSender, Instant> senderLastReport = new WeakHashMap<>();
 
-    @Inject ReportCommands(QueryService<Report> reportService,
-                           ReportCreator reportCreator,
+    @Inject ReportCommands(ReportCreator reportCreator,
                            ReportConfiguration reportConfiguration,
-                           UserFinder userFinder,
-                           SyncExecutor syncExecutor,
-                           Server localServer,
-                           BukkitUserStore userStore,
                            Audiences audiences,
-                           IdentityProvider identities,
                            AdminChannel adminChannel) {
-        this.reportService = reportService;
         this.reportCreator = reportCreator;
         this.reportConfiguration = reportConfiguration;
-        this.userFinder = userFinder;
-        this.syncExecutor = syncExecutor;
-        this.localServer = localServer;
-        this.userStore = userStore;
         this.audiences = audiences;
-        this.identities = identities;
         this.adminChannel = adminChannel;
     }
 
@@ -98,7 +60,7 @@ public class ReportCommands implements Commands, Listener {
     public void report(final CommandContext args, final CommandSender sender) throws CommandException {
         assertEnabled();
 
-        // check for cooldown expiration
+        // Check for cooldown expiration
         if(!sender.hasPermission(ReportPermissions.COOLDOWN_EXEMPT)) {
             final Instant lastReportTime = senderLastReport.get(sender);
             if (lastReportTime != null) {
@@ -109,11 +71,12 @@ public class ReportCommands implements Commands, Listener {
             }
         }
 
+        // Check if player online
         if (sender.getServer().getPlayerExact(args.getString(0), sender) == null) {
             throw new TranslatableCommandException("command.playerNotFound");
         }
 
-        ReportCreator reportCreator = new ReportCreator(this.identities);
+        // Send report to staff
         adminChannel.viewers()
                 .filter(viewer ->
                         viewer.hasPermission(ReportPermissions.RECEIVE))
